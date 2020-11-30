da869d1637305ee8755a8350c05eeb74fceac267
Buckley Ross <brr322@lehigh.edu>
2020-11-30 00:38:22 -0500

    fix(10): remove whitespace from env vars
    Signed-off-by: Buckley Ross <brr322@lehigh.edu>
    Change-Id: Ib613597f20cbbfd21be56ec171a62fb02f73b43f

--- utils/postgres.js
+++ utils/postgres.js
@@ -1,11 +1,11 @@
 // use node-postgres module to create a pool of connections w/ environment variables stored off-repo
 const Pool = require('pg').Pool;
 const pool = new Pool({
-	user: process.env['POSTGRES_USER'],
-	password: process.env['POSTGRES_PASS'],
-	host: process.env['POSTGRES_HOST'],
-	port: process.env['POSTGRES_PORT'],
-	database: process.env['POSTGRES_DB'],
+	user: process.env['POSTGRES_USER'].replace(/\s/g, ''),
+	password: process.env['POSTGRES_PASS'].replace(/\s/g, ''),
+	host: process.env['POSTGRES_HOST'].replace(/\s/g, ''),
+	port: process.env['POSTGRES_PORT'].replace(/\s/g, ''),
+	database: process.env['POSTGRES_DB'].replace(/\s/g, ''),
 	ssl: {
 		rejectUnauthorized: false
 	}
