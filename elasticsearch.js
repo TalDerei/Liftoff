9d90a96e07865e2984c99dae8faa8153d0809212
Tal Derei <Tad222@lehigh.edu>
2020-11-29 16:05:34 -0800

    creating elasticsearch cluster

---
+++ elasticsearch.js
@@ -0,0 +1,14 @@
+/* connect to elasticsearch cluster instance */
+
+var elasticsearch = require('elasticsearch');
+
+var client = new elasticsearch.Client( {  
+    hosts: [
+      'https://[username]:[password]@[server]:[port]/',
+      'https://[username]:[password]@[server]:[port]/'
+    ]
+  });
+  
+  module.exports = client; 
+
+  
