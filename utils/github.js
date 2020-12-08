fd24cac636d63b5b803429efa54e4ccd755a358f
Alan Zarza <aez222@lehigh.edu>
2020-12-07 20:06:48 -0500

    feature(19): I think this is what we want

--- utils/github.js
+++ utils/github.js
@@ -1,15 +1,49 @@
-function data(owner,repo){
-    fetch('https://api.github.com/repos/'+owner+'/'+repo+'/languages',{
-        method: 'GET',
-        headers:{
-            'Accept': 'application/vnd.github.v3+json'
-        },
-    })
+function dataFetch(search){
+    //search should be in form ':owner/:repo'
+    return fetch('https://api.github.com/repos/'+search+'/commits')
     .then(data=>data.json())
     .then(data=>{
-        console.log(data)
+
+    //collect the commit ids
+    let commitsID = [];
+    for(i = 0; i < data.length; i++){
+      commitsID.push(data[i].sha)
+    }
+
+    //make group of fetches
+    let requests = commitsID.map(id=>{
+      return fetch('https://api.github.com/repos/'+search+'/commits/'+id)
     })
+
+    //parse the fetches
+    return Promise.all(requests)
+    .then(body=>{
+      let requests = body.map(res=>{
+        return res.json()
+      })
+      return Promise.all(requests)
+      .then(data=>{
+        //now we have the new and improved data, but still need language info
+        return fetch('https://api.github.com/repos/'+search+'/languages')
+        .then(data=>data.json())
+        .then(lang=>{
+          let finalData = {'commits': data,'languages':lang}
+          console.log(finalData)
+          return finalData
+        })
+        .catch(error=>{
+          console.error('Error:',error)
+        })
+      })
+    })
+    .catch(error=>{
+      console.error('Error:',error)
+    })
+  })
+  .catch(error=>{
+    console.error('Error:',error)
+  })
 }
 
 
-module.exports = data;
+module.exports = dataFetch;
