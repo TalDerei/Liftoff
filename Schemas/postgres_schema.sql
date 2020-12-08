a5829c327c1493dc017339c0f6a93f93380f925d
Buckley Ross <brr322@lehigh.edu>
2020-12-08 00:48:07 -0500

    fix(28): fix postgres schema
    I had to add primary key constraints to the users and groups tables, and
    also fix the foreign key constrains syntax for the membership table, but
    I tested it all against a local PostgreSQL instance, and it seems to
    work ok now.
    
    Signed-off-by: Buckley Ross <brr322@lehigh.edu>
    Change-Id: I6a9a832fd67674959f4e047c5862577ae157d0b1

--- Schemas/postgres_schema.sql
+++ Schemas/postgres_schema.sql
@@ -1,29 +1,28 @@
-
 CREATE TYPE permission_level AS ENUM(
-    'Administrator',
-    'Maintainer',
-    'Contributor'
+	'Administrator',
+	'Maintainer',
+	'Contributor'
 );
-
 
 CREATE TABLE IF NOT EXISTS users (
-    username varchar(255),
-    email varchar(255),
-    realname varchar(255)
+	username varchar(255),
+	email varchar(255),
+	realname varchar(255),
+	PRIMARY KEY(username)
 );
 
-
 CREATE TABLE IF NOT EXISTS groups (
-    groupname varchar(255),
-    grouptitle varchar(255)
-
+	groupname varchar(255),
+	grouptitle varchar(255),
+	PRIMARY KEY(groupname)
 );
 
 CREATE TABLE IF NOT EXISTS group_membership (
-    groupname varchar(255),
-    username varchar(255),
-    permission permission_level,
-    PRIMARY KEY(groupname, username),
-    FOREIGN KEY(groupname) REFERENCES groups.groupname,
-    FOREIGN KEY(username) REFERENCES users.username 
+	groupname varchar(255),
+	username varchar(255),
+	permission permission_level,
+	PRIMARY KEY(groupname, username),
+	FOREIGN KEY(groupname) REFERENCES groups(groupname),
+	FOREIGN KEY(username) REFERENCES users(username)
 );
+
