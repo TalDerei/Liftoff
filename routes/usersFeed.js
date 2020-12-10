43bfcc86fec59ad387d10ca5b9220f0c448370a9
Nick <nlembo99@gmail.com>
2020-12-10 17:02:44 -0500

    added routes for elasticsearch query

--- routes/usersFeed.js
+++ routes/usersFeed.js
@@ -1,2 +1,33 @@
 /* Routes associated with querying a user's feed */
 
+const express = require('express');
+const router = express.Router();
+let client = require('../utils/elastic/connection.js');
+
+
+router.get('/', (req,res) => {
+  let search = req.query.q;
+  if (search == undefined || search == null)
+    res.status(501).send();
+  client.search({
+    index: 'projects',
+    body: {
+      query: {
+          multi_match: {
+            "query": search,
+            "fields": ["tags", "description", "title"],
+          }
+      }
+    }
+  },
+  (error, elastic_res) => {
+    if (err) {
+    	console.log(error);
+	res.status(501).send();
+    }
+    res.json(elastic_res.hits.hits[0]);
+
+  });
+});
+
+module.exports = router;
