8eb73237d7058f5ad0a1052061529860e28e688e
Sam Reid Markovich <srm322@lehigh.edu>
2020-12-11 00:48:29 -0500

    One line description, all tags shown

--- web/scripts/search.js
+++ web/scripts/search.js
@@ -4,22 +4,19 @@ const results = document.getElementById('search-results');
 
 submitButton.addEventListener('click', (e) => {
   console.log(e.target.value);
-  //fetch for group info using project name
-  fetch(`https://liftoff.acm.lehigh.edu/project/${e.target.value}`)
+  fetch(`https://liftoff.acm.lehigh.edu/api/project/${e.target.value}`)
     .then(response => response.json())
     .then(data => {
       console.log(data);
       let resultsHTML = "";
-      //for loop for results
       for (let result of data) {
         let tagsHTML = "";
-        //for loop to generate html for tags
+        var tagsList = result.tags.join();
         for (let tag of result.tags.split(',')) {
           tagsHTML += `
             <li class="tag">${tag.trim()}</li>
           `;
         }
-        //results HTML
         resultsHTML += `
           <li>
             <h3 class="group-title">${result.title}</h3>
@@ -32,3 +29,4 @@ submitButton.addEventListener('click', (e) => {
       results.innerHTML = resultsHTML;
     });
 });
+
